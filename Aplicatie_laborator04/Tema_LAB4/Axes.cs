a9256c399f651bbbe82c03e1d62cc63496928843
CosminP07 <cosmin.pintilei@student.usv.ro>
2024-11-08 12:50:35 +0200

    Tema_laborator04
    Am implementat aplicatia din video-ul cu numarul 5

---
+++ Aplicatie_laborator04/Tema_LAB4/Axes.cs
@@ -0,0 +1,53 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Tema_LAB4
+{
+    class Axes
+    {
+        private bool myVisibility;
+        private const int AXIS_LENGTH = 75;
+        public Axes()
+        {
+            myVisibility = true;
+        }
+        public void Draw()
+        {
+            if(myVisibility)
+            {
+                GL.LineWidth(3.0f);
+
+                GL.Begin(PrimitiveType.Lines);
+                GL.Color3(Color.Red);
+                GL.Vertex3(0, 0, 0);
+                GL.Vertex3(AXIS_LENGTH, 0, 0);
+                GL.Color3(Color.ForestGreen);
+                GL.Vertex3(0, 0, 0);
+                GL.Vertex3(0, AXIS_LENGTH, 0);
+                GL.Color3(Color.RoyalBlue);
+                GL.Vertex3(0, 0, 0);
+                GL.Vertex3(0, 0 , AXIS_LENGTH);
+                GL.End();
+
+                GL.LineWidth(1.0f);
+            }
+        }
+        public void Show()
+        {
+            myVisibility = true;
+        }
+        public void Hide()
+        {
+            myVisibility = false;
+        }
+        public void ToggleVisibility()
+        {
+            myVisibility = !myVisibility;
+        }
+    }
+}
---
+++ Aplicatie_laborator04/Tema_LAB4/Window3D.cs
@@ -0,0 +1,193 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace Tema_LAB4
+{
+
+    class Window3D : GameWindow
+    {
+        private KeyboardState previousKeyboard;
+        private MouseState previousMouse;
+        private readonly Randomizer rando;
+        private readonly Axes ax;
+        private readonly Grid grid;
+        private readonly Camera3DIsometric cam;
+        private List<Objectoid> rainofObjects;
+        private bool GRAVITY = true;
+        // DEFAULTS
+        private readonly Color DEFAULT_BKG_COLOR = Color.FromArgb(49, 50, 51);
+
+        public Window3D() : base(1280, 768, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+            // inits
+            rando = new Randomizer();
+            ax = new Axes();
+            grid = new Grid();
+            cam = new Camera3DIsometric();
+
+            rainofObjects = new List<Objectoid>();
+
+            DisplayHelp();
+
+
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.ClearColor(DEFAULT_BKG_COLOR);
+            GL.Viewport(0, 0, this.Width, this.Height);
+            Matrix4 perspectiva = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 1024);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiva);
+
+            // set the eye
+            cam.SetCamera();
+
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            
+
+            // LOGIC CODE
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            MouseState currentMouse = Mouse.GetState();
+
+            if (currentKeyboard[Key.Escape])
+            {
+                Exit();
+            }
+
+            if (currentKeyboard[Key.H] && !previousKeyboard[Key.H])
+            {
+                DisplayHelp();
+            }
+
+            if (currentKeyboard[Key.R] && !previousKeyboard[Key.R])
+            {
+                GL.ClearColor(DEFAULT_BKG_COLOR);
+                ax.Show();
+                grid.Show();
+            }
+
+            if (currentKeyboard[Key.K] && !previousKeyboard[Key.K])
+            {
+                ax.ToggleVisibility();
+            }
+
+            if (currentKeyboard[Key.B] && !previousKeyboard[Key.B])
+            {
+                GL.ClearColor(rando.RandomColor());
+            }
+
+            if (currentKeyboard[Key.V] && !previousKeyboard[Key.V])
+            {
+                grid.ToggleVisibility();
+            }
+
+            
+
+            // camera control (isometric mode)
+            if (currentKeyboard[Key.W])
+            {
+                cam.MoveForward();
+            }
+            if (currentKeyboard[Key.S])
+            {
+                cam.MoveBackward();
+            }
+            if (currentKeyboard[Key.A])
+            {
+                cam.MoveLeft();
+            }
+            if (currentKeyboard[Key.D])
+            {
+                cam.MoveRight();
+            }
+            if (currentKeyboard[Key.Q])
+            {
+                cam.MoveUp();
+            }
+            if (currentKeyboard[Key.E])
+            {
+                cam.MoveDown();
+            }
+
+            if (currentMouse[MouseButton.Left] && !previousMouse[MouseButton.Left])
+            {
+                rainofObjects.Add(new Objectoid(GRAVITY));
+            }
+            if (currentMouse[MouseButton.Right] && !previousMouse[MouseButton.Right])
+            {
+                rainofObjects.Clear();
+            }
+            if (currentKeyboard[Key.G] && !previousKeyboard[Key.G])
+            {
+                GRAVITY = !GRAVITY;
+            }
+
+            foreach (Objectoid obj in rainofObjects)
+            {
+                obj.UpdatePosition(GRAVITY);
+            }
+
+            previousKeyboard = currentKeyboard;
+            previousMouse = currentMouse;
+            // END logic code
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            // RENDER CODE
+            grid.Draw();
+            ax.Draw();
+            
+            foreach(Objectoid obj in rainofObjects)
+            {
+                obj.Draw();
+            }
+
+            // END render code
+
+            SwapBuffers();
+        }
+
+        private void DisplayHelp()
+        {
+            Console.WriteLine("\n      MENIU");
+            Console.WriteLine(" (H) - meniul");
+            Console.WriteLine(" (ESC) - parasire aplicatie");
+            Console.WriteLine(" (K) - schimbare vizibilitate sistem de axe");
+            Console.WriteLine(" (R) - resteaza scena la valori implicite");
+            Console.WriteLine(" (B) - schimbare culoare de fundal");
+            Console.WriteLine(" (V) - schimbare vizibilitate linii");
+            Console.WriteLine(" (W,A,S,D,  Q,  E) - deplasare camera (izometric)");
+            Console.WriteLine(" (G) - manipuleaza gravitatia");
+            Console.WriteLine(" (Mouse click stanga) - genereaza un nou obiect la o inaltime aleatoare");
+            Console.WriteLine(" (Mouse click dreapta) - curata lista de obiecte");
+        }
+    }
+}
